197ac49b01febfca0d1b056a0889e47022e48ca8
gerald_xyz <gerald.cainicela.a@gmail.com>
2025-11-07 16:09:35 -0500

    Add visor main  and sub

--- test/src/rtsp_sub.cpp
+++ test/src/rtsp_sub.cpp
@@ -6,11 +6,15 @@ int main() {
     std::string pass = "Panto2025";
     std::string ip = "192.168.0.101";        
     int port = 554;
-    std::string stream_type = "sub";    
+    std::string stream_type_main = "main";   
+    std::string stream_type_sub = "sub";  
 
     // crear y ejecutar el visor
-    StreamViewer viewer(user, pass, ip, port, stream_type);
-    viewer.run();
+    StreamViewer viewer_main(user, pass, ip, port, stream_type_main);
+    StreamViewer viewer_sub(user, pass, ip, port, stream_type_sub);
+    viewer_main.run();
+    viewer_sub.run();
+
 
     return 0;
 }
