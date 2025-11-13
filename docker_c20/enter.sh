9dce5e490e5c3489a715f9bb166ec53a37d47241
gerald_xyz <gerald.cainicela.a@gmail.com>
2025-11-13 02:03:12 -0500

    delete docker no used and rename folder

--- docker_c20/enter.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-CONTAINER_NAME="l4t-cpp-20"
-
-if [ ! "$(docker ps -q -f name=${CONTAINER_NAME})" ]; then
-    echo "Contenedor no está corriendo"
-    exit 1
-fi
-
-docker exec -it -u root -w /workspace ${CONTAINER_NAME} bash
