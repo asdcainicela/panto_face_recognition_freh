4795117f981e131e7ddaaab8bb1664e74781485f
gerald_xyz <gerald.cainicela.a@gmail.com>
2025-11-25 14:54:45 -0500

    refactor

--- system/main.cpp
+++ system/main.cpp
@@ -486,13 +486,13 @@ int main(int argc, char* argv[]) {
                 //auto id_temporal = f.id*10+std::rand()%10; // ID temporal para pruebas
 
                 //std::string line1 = "ID:" + std::to_string("fhkap95817"); //std::to_string(f.id);
-                std::string line1 = "ID:" + std::string("371") + std::string("Buscando...");
+                std::string line1 = "ID:" + std::string("gpx371") + std::string("Buscando...");
                 bool nuevo =  true;
                 if (nuevo) {
-                    line1 = "ID:" + std::string("371")+  std::string(" (NUEVO) ");
+                    line1 = "ID:" + std::string("gpx371")+  std::string(" (NUEVO) ");
                 }
                 else{
-                    line1 = "ID:" + std::to_string("480")+  "-> ID:" + std::string("371") + std::string(" (RECOMPRA) ");
+                    line1 = "ID:" + std::to_string("jpe480")+  "-> ID:" + std::string("gpx371") + std::string(" (RECOMPRA) ");
                 }
 
                 if (f.is_recognized && draw_names) {
